0c33a1558a6ff69e6be23db7737299a971b0c78e
chasa <79016507+itschasa@users.noreply.github.com>
2022-06-19 17:05:53 +0100

    Version update (1.0.2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hotmailbox.py',
-    version='1.0.1',    
+    version='1.0.2',    
     description='An API wrapper for hotmailbox.me written in Python.',
     url='https://github.com/itschasa/hotmailbox.py',
     author='itschasa',
